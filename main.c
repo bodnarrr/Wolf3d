9d1188163729f6be3b5d31b9bc042cf09bd32f4d
Andrii Bodnar <a.bodnarrr@gmail.com>
2019-04-19 13:41:54 +0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,7 @@
+
+#include "wolf3d.h"
+
+int		main(int ac, char **av)
+{
+	ft_printf("Hello, Wolf3d!\n");
+}
---
+++ wolf3d.h
@@ -0,0 +1,7 @@
+
+#ifndef WOLF3D_H
+# define WOLF3D_H
+
+#include "libftprintf/libftprintf.h"
+
+#endif
