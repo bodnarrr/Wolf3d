762c3af3cbc27c5e305dbc7263cb8513a7f99a34
Andrii Bodnar <a.bodnarrr@gmail.com>
2019-04-19 17:14:18 +0300

    Open window with key hooks

--- main.c
+++ main.c
@@ -1,7 +1,36 @@
 
 #include "wolf3d.h"
 
-int		main(int ac, char **av)
-{
-	ft_printf("Hello, Wolf3d!\n");
+int		main(int ac, char **av) {
+
+	bool		is_working = 1;
+	SDL_Event	event;
+	SDL_Window	*window = NULL;
+	SDL_Surface	*screenSurface = NULL;
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+	else
+	{
+		window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+		if (window == NULL)
+			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+		else
+		{
+			screenSurface = SDL_GetWindowSurface(window);
+			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0x7F, 0xFF, 0xFF));
+			SDL_UpdateWindowSurface(window);
+		}
+	}
+	while (is_working && SDL_WaitEvent(&event))
+	{
+		if ((event.type == SDL_QUIT) || (event.type == SDL_KEYDOWN
+									 && event.key.keysym.scancode == SDL_SCANCODE_ESCAPE))
+			is_working = 0;
+		else if (event.type == SDL_KEYDOWN)
+			ft_printf("Code = %d\n", event.key.keysym.scancode);
+	}
+
+	ft_printf("EXITING");
+	return 0;
 }
--- wolf3d.h
+++ wolf3d.h
@@ -2,6 +2,12 @@
 #ifndef WOLF3D_H
 # define WOLF3D_H
 
-#include "libftprintf/libftprintf.h"
+# include "libftprintf/libftprintf.h"
+# include "/usr/local/Cellar/sdl2/2.0.9_1/include/SDL2/SDL.h"
+# include "/usr/local/Cellar/sdl2/2.0.9_1/include/SDL2/SDL_events.h"
+# include <stdbool.h>
+
+# define SCREEN_WIDTH 640
+# define SCREEN_HEIGHT 480
 
 #endif
